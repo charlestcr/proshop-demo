3552eff1b035e208ec30e03c6d399e9ffc3522b4
charlestcr <charlesmjmtm@gmail.com>
2022-03-29 15:55:16 +0530

    hh

--- backend/settings.py
+++ backend/settings.py
@@ -147,12 +147,11 @@ TEMPLATES = [
         },
     },
 ]
-
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases fgfg
 
 DATABASES = {
     'default': {
