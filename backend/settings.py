1a2ca3fd16643f8fbb56bd7b5ce0f934a55b9118
charlestcr <charlesmjmtm@gmail.com>
2022-03-29 17:31:25 +0530

    dd
    dd

--- backend/settings.py
+++ backend/settings.py
@@ -134,7 +134,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'frontend/build')
+            os.path.join(BASE_DIR, 'frontend1/build')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
